2cd06615c3f7c3984b33e89713a6f54d58b0004d
la2697 <56022392+la2697@users.noreply.github.com>
2020-11-02 12:07:53 +0100

    Mit Math Optionen

--- Zahnräder_v2/Berechnung von Zahnrädern/Program.cs
+++ Zahnräder_v2/Berechnung von Zahnrädern/Program.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
         
 namespace Berechnung_von_Zahnrädern{
     class Program{
-        public const double Pi   = 3.141592654;
+        public const double Pi   = Math.PI;
         public const double S235JR = 0.00785; // Dichte S235JR 
         public const double dGJL = 7.15/1000; //Dichte EN-GJL-200
         public const double dGJS = 7.1/1000 ; //Dichte EN-GJS-350
@@ -17,19 +17,21 @@ namespace Berechnung_von_Zahnrädern{
         public const double c = 0.167; // Kopfspiel Standard
         public const double c1 = 0.1; // Kopfspiel DIN 867
         public const double c2 = 0.3; // Kopfspiel DIN 867
-        public const double cosα = 0.34906585; // Normverzahnung cos20°
+        public static double cosα = Math.Cos(20); // Normverzahnung cos20°
+       
 
         static void Main(string[] args){
             Console.WriteLine("BERECHNUNG DER ZAHNRADGEOMETRIE\n-------------------------------");
             Console.Write("Geben Sie ihr Modul ein:                ");
             double m = Convert.ToDouble(Console.ReadLine());
+            
             Console.Write("Geben Sie ihre Zahnzahl ein:            ");
             double z = Convert.ToDouble(Console.ReadLine());
             double d = m * z;
             Console.Write("Geben Sie an wie Breit das Zahnrad ist: ");
-            double b = Convert.ToDouble(Console.ReadLine()); 
+            double b = Convert.ToDouble(Console.ReadLine());
 
-            double A=((d / 2) * (d / 2) * Pi);
+            double A =Math.Round(d / 2) * (d / 2) * Math.PI;
 
             int material = 0;
             while (material < 1 || material > 3){
@@ -38,7 +40,7 @@ namespace Berechnung_von_Zahnrädern{
                 if (material < 1 || material > 3) Console.WriteLine("\nFEHLER: Ungueltige Eingabe");
             }
 
-            int eingabe = 0;
+            int eingabe =  0;
             while (eingabe < 1 || eingabe > 3){
                 Console.Write("\nWählen Sie das Kopfspiel\n1: Standardkopfspiel\n2: Kopfspiel 0.1 DIN 867\n3: Kopfspiel 0.3 DIN 867\nAuswahl:");
                 eingabe = Convert.ToInt16(Console.ReadLine());
@@ -47,48 +49,52 @@ namespace Berechnung_von_Zahnrädern{
 
             switch (eingabe){
                 case 1:
-                    Console.WriteLine("\nZahnfußhöhe mit Standardkopfspiel in mm:               " + (m + c) + "mm");
-                    Console.WriteLine("\nFußkreisdurchmesser mit Standardkopfspiel in mm:       " + (m * z) + 2 * (m + c) + "mm");
-                    Console.WriteLine("\nZahnhöhe mit Standardkopfspiel in mm:                  " + (2*m+c) + "mm");
+                    Console.WriteLine("\nZahnfußhöhe mit Standardkopfspiel in mm:               " + Math.Round(m + c) + "mm");
+                    Console.WriteLine("\nFußkreisdurchmesser mit Standardkopfspiel in mm:       " + Math.Round(m * z) + 2 * (m + c) + "mm");
+                    Console.WriteLine("\nZahnhöhe mit Standardkopfspiel in mm:                  " + Math.Round(2*m+c) + "mm");
                     break;
 
                 case 2:
-                    Console.WriteLine("\nZahnfußhöhe mit Kopfspiel 0.1 DIN 867 in mm:           " + (m + c1) + "mm");
-                    Console.WriteLine("\nFußkreisdurchmesser mit Kopfspiel 0.1 DIN 867 in mm:   " + (m * z) + 2 * (m + c1) + "mm");
-                    Console.WriteLine("\nZahnhöhe mit Kopfspiel 0.1 DIN 867 in mm:              " + (2 * m + c1) + "mm");
+                    Console.WriteLine("\nZahnfußhöhe mit Kopfspiel 0.1 DIN 867 in mm:           " + Math.Round(m + c1) + "mm");
+                    Console.WriteLine("\nFußkreisdurchmesser mit Kopfspiel 0.1 DIN 867 in mm:   " + Math.Round(m * z) + 2 * (m + c1) + "mm");
+                    Console.WriteLine("\nZahnhöhe mit Kopfspiel 0.1 DIN 867 in mm:              " + Math.Round(2 * m + c1) + "mm");
                     break;
 
                 case 3:
-                    Console.WriteLine("\nZahnfußhöhe mit Kopfspiel 0.3 DIN 867 in mm:           " + (m + c2) + "mm");
-                    Console.WriteLine("\nFußkreisdurchmesser mit Kopfspiel 0.3 DIN 867 in mm:   " + (m * z) + 2 * (m + c2) + "mm");
-                    Console.WriteLine("\nZahnhöhe mit Kopfspiel 0.3 DIN 867 in mm:              " + (2 * m + c2) + "mm");
+                    Console.WriteLine("\nZahnfußhöhe mit Kopfspiel 0.3 DIN 867 in mm:           " + Math.Round(m + c2) + "mm");
+                    Console.WriteLine("\nFußkreisdurchmesser mit Kopfspiel 0.3 DIN 867 in mm:   " + Math.Round(m * z) + 2 * (m + c2) + "mm");
+                    Console.WriteLine("\nZahnhöhe mit Kopfspiel 0.3 DIN 867 in mm:              " + Math.Round(2 * m + c2) + "mm");
                     break;
             }
 
-            Console.WriteLine("\nTeilung der Zähne am Zahnrad in mm:                    " + Pi * m + "mm");
-            Console.WriteLine("\nGrundkreisdurchmesser des Zahnrades in mm:             " + (m * z * cosα) + "mm");
+            Console.WriteLine("\nTeilung der Zähne am Zahnrad in mm:                    " + Math.Round(Pi * m) + "mm");
+            Console.WriteLine("\nGrundkreisdurchmesser des Zahnrades in mm:             " + Math.Round(m * z * cosα) + "mm");
             Console.WriteLine("\nZahnkopfhöhe ist das Modul:                            " + m + "mm");
 
-            double V = A * b * dGJL;
-            double V1 = A * b * dGJS;
-            double V2 = A * b * S235JR;
+            double V = Math.Round(A * b * dGJL);
+            double V1 = Math.Round(A * b * dGJS);
+            double V2 = Math.Round(A * b * S235JR);
 
             switch (material){
                 case 1:
-                    Console.WriteLine("\nGewicht:                                               " + (A * b * dGJL) + "g");
-                    Console.WriteLine("\nPreis:                                                 " + (V * dGJL) + "Euro");
+                    Console.WriteLine("\nGewicht:                                               " + Math.Round(A * b * dGJL) + "g");
+                    Console.WriteLine("\nPreis:                                                 " + V * dGJL + "Euro");
                     break;
                     
                 case 2:
-                    Console.WriteLine("\nGewicht:                                               " + (A * b * dGJS) + "g");
-                    Console.WriteLine("\nPreis:                                                 " + (V1 * dGJS) + "Euro");
+                    Console.WriteLine("\nGewicht:                                               " + Math.Round(A * b * dGJS) + "g");
+                    Console.WriteLine("\nPreis:                                                 " + V1 * dGJS + "Euro");
                     break;
 
                 case 3:
-                    Console.WriteLine("\nGewicht:                                               " + (A * b * S235JR) + "g");
-                    Console.WriteLine("\nPreis:                                                 " + (V2 * S235JR) + "Euro");
+                    Console.WriteLine("\nGewicht:                                               " + Math.Round(A * b * S235JR) + "g");
+                    Console.WriteLine("\nPreis:                                                 " + V2 * S235JR + "Euro");
                     break;
+
+
+                
             }
+            Console.ReadKey();
         }
     }
 }
